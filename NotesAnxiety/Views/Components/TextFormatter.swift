4d6efeb15ab28b56de8634b46a7928b80327061c
senakristiawan! <senakristiawan1@gmail.com>
2024-07-12 09:56:56 +0700

    Keyboard Text Format Modal

---
+++ NotesAnxiety/Views/Components/TextFormatter.swift
@@ -0,0 +1,18 @@
+//
+//  TextFormatter.swift
+//  NotesAnxiety
+//
+//  Created by Sena Kristiawan on 11/07/24.
+//
+
+import SwiftUI
+
+struct TextFormatter: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TextFormatter()
+}
