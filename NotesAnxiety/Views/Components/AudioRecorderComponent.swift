a7d179d95388e4aa84a44136733a0bd8e544a4a2
senakristiawan! <senakristiawan1@gmail.com>
2024-07-28 18:03:02 +0700

    Improved Voice Record UI

--- NotesAnxiety/Views/Components/AudioRecorderComponent.swift
+++ NotesAnxiety/Views/Components/AudioRecorderComponent.swift
@@ -13,22 +13,86 @@ struct AudioRecorderComponent: View {
     @State private var recording = false
     @State private var audioRecorder: AVAudioRecorder?
     @State private var audioPlayer: AVAudioPlayer?
+    @State private var isPulsing = false
 
     var body: some View {
         VStack {
             if recording {
-                Button("Stop Recording") {
+                Text("Tap to stop record..")
+                    .foregroundStyle(Color.red)
+                    .font(.system(size: 18))
+                    .padding(EdgeInsets(top: 0, leading: 0, bottom: 40, trailing: 0))
+                
+                Button(action: {
                     stopRecording()
+                    isPulsing = false
+                }){
+                    ZStack{
+                        Circle()
+                            .fill(Color.red)
+                            .opacity(0.3)
+                            .frame(width: 150, height: 150)
+                            
+                        Circle()
+                            .fill(Color.red)
+                            .scaleEffect(isPulsing ? 1.5 : 1.0)
+                            .opacity(isPulsing ? 0 : 0.2)
+                            .frame(width: 150, height: 150)
+                            .animation(Animation.easeInOut(duration: 1.0).repeatForever(autoreverses: true), value: isPulsing)
+                     
+                        Image(systemName: "stop.fill")
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .frame(width: 50, height: 50)
+                            .foregroundStyle(Color.red)
+                    }
+                    .onAppear{
+                        isPulsing = true
+                }
                 }
             } else {
-                Button("Start Recording") {
+                Text("Tap to start record..")
+                    .foregroundStyle(Color.red)
+                    .font(.system(size: 18))
+                    .padding(EdgeInsets(top: 0, leading: 0, bottom: 40, trailing: 0))
+                
+                Button(action: {
                     startRecording()
+                    isPulsing = false
+                }){
+                    ZStack{
+                        Circle()
+                            .fill(Color.red)
+                            .opacity(0.3)
+                            .frame(width: 150, height: 150)
+
+                        Circle()
+                            .fill(Color.red)
+                            .scaleEffect(isPulsing ? 2 : 1.0)
+                            .opacity(isPulsing ? 0 : 0.2)
+                            .frame(width: 150, height: 150)
+                            .animation(Animation.easeInOut(duration: 1.0).repeatForever(autoreverses: true), value: isPulsing)
+                     
+                        Image(systemName: "mic.fill")
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .frame(width: 50, height: 50)
+                            .foregroundStyle(Color.red)
+                    }
+                    .onAppear{
+                        isPulsing = true
+                }
                 }
             }
             
             if let audioFilename = audioFilename {
-                Button("Play Recording") {
+                Button(action: {
                     playRecording(url: audioFilename)
+                }){
+                    Image(systemName: "play.circle.fill")
+                        .foregroundStyle(Color.red)
+                        .font(.system(size: 60))
+                        .padding(EdgeInsets(top: 20, leading: 0, bottom: 0, trailing: 0))
                 }
             }
         }
